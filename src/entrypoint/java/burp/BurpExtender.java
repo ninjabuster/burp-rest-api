1c805924570f30cebb6b9fd0016df2cab5138184
Andrea Brancaleoni <abc@pompel.me>
2018-10-31 17:19:33 +0100

    Fix burp classloading on Linux

--- src/entrypoint/java/burp/BurpExtender.java
+++ src/entrypoint/java/burp/BurpExtender.java
@@ -7,6 +7,7 @@ package burp;
 import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Iterator;
 
 /**
  * burp.BurpExtender is the burp-rest-api 2nd-gen entrypoint.
@@ -35,12 +36,34 @@ public class BurpExtender implements IBurpExtender {
     }
 
     private static void legacyRegisterExtenderCallbacks(IBurpExtenderCallbacks callbacks)
-            throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
-
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Class clazz = classLoader.loadClass("burp.LegacyBurpExtender");
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+        Class clazz = loadClass("burp.LegacyBurpExtender");
         Object obj = clazz.newInstance();
         Method method = clazz.getMethod("registerExtenderCallbacks", IBurpExtenderCallbacks.class);
         method.invoke(obj, callbacks);
+    }
+
+    private static Class loadClass(String name) throws ClassNotFoundException{
+        Class clazz = null;
+        Iterator<Thread> tit = Thread.getAllStackTraces().keySet().iterator();
+        ClassLoader i = Thread.currentThread().getContextClassLoader();
+
+        while (clazz == null && i != null) {
+            try {
+                clazz = i.loadClass(name);
+                clazz.newInstance();
+            } catch (ClassNotFoundException|InstantiationException|IllegalAccessException|NoClassDefFoundError e) {
+                clazz = null;
+            }
+            i = i.getParent();
+            if (i == null && tit.hasNext())
+                i = tit.next().getContextClassLoader();
+        }
+
+        if (clazz == null) {
+            throw new ClassNotFoundException("loadClass cannot find the class in any thread.");
+        }
+
+        return clazz;
     }
 }
